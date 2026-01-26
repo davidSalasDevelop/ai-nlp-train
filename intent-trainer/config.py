eec665a6f6f9391f8e6668646ee2617148517444
davidSalasDevelop <davidmsnsalas@hotmail.com>
2026-01-26 18:26:04 +0000

    saving progress

--- intent-trainer/config.py
+++ intent-trainer/config.py
@@ -10,7 +10,7 @@ MODEL_NAME = "prajjwal1/bert-tiny"
 DATASET_PATH = 'dataset/dataset_v2.json'
 
 # Directory to save the final, best-performing model
-FINAL_MODEL_OUTPUT_DIR = "output-models"
+FINAL_MODEL_OUTPUT_DIR = "../output-models"
 
 # MLflow configuration (used as environment variables)
 MLFLOW_TRACKING_URI = "http://138.197.233.39:4200"
